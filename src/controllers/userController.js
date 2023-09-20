84eddf27a08daea4fc0dccedb54246d3622a719a
Amirsohil123 <amir77766618@gmail.com>
2023-09-20 14:02:33 +0530

    hi

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -5,7 +5,7 @@ const userModel = require("../models/userModel")
 const createUser = async(req,res)=>{
     
     const data = req.body
-   const  {name,phone,email,password,address} = user
+   const  {name,phone,email,password,address} = data
    const {street, city, state} = data.address
     if(!name || !phone || !email || !password || !address || !street || !city || !state){
         res.status(400).send({status:false,message:"Please under all the details"})
